9ff3782669b483e72ca8b08c57f10d24e44bccf0
youssef shaaban <ysfme2@hotmail.com>
2023-09-07 02:14:05 +0300

    comment

---
+++ NewInCSharp8/3-PatternMatchingEnhancements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewInCSharp8
+{
+    internal class PatternMatchingEnhancements
+    {
+        public void DeclarationAndTypePatterns()
+        {
+            object str = "Hello World!";
+            if (str is string greeting)
+            {
+                Console.WriteLine(greeting);
+            }
+
+        }
+        public static string GetType(object obj) => obj switch
+        {
+            string => "string",
+            int => "integer",
+
+            null => throw new ArgumentNullException(nameof(obj)),
+            _ => throw new ArgumentException("Unknown type of a object", nameof(obj)),
+        };
+
+        public string GetSign(double number) => number switch
+        {
+            < 0 => "Negative",
+            > 0 => "Positive",
+            0 => "Zero",
+            double.NaN => "Not a number",
+            _ => throw new ArgumentException("Unknown double number ")
+        };
+    }
+}

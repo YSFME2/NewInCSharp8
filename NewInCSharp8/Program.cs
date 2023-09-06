af4d4a20f6b64543208156e9fe730607ddceefa0
youssef shaaban <ysfme2@hotmail.com>
2023-09-07 01:13:26 +0300

    Add project files.

---
+++ NewInCSharp8/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NewInCSharp8
+{
+    struct XX
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public readonly int GetSum()
+        {
+            Change();
+            return X + Y;
+        }
+        public void Change()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            XX xx = new XX();
+            xx.X = 45; xx.Y = 15;
+            Console.WriteLine("Hello, World!" + xx.GetSum());
+        }
+    }
+}

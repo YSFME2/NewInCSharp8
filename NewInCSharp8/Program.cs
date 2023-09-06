8e80601126d2124967bf419b8c234a3958c67289
youssef shaaban <ysfme2@hotmail.com>
2023-09-07 01:18:50 +0300

    readonly members

--- NewInCSharp8/Program.cs
+++ NewInCSharp8/Program.cs
@@ -4,6 +4,16 @@
     {
         public int X { get; set; }
         public int Y { get; set; }
+        private int z;
+        public int Z
+        {
+            readonly get
+            {
+                Change();
+                return z;
+            }
+            set { z = value; }
+        }
         public readonly int GetSum()
         {
             Change();
